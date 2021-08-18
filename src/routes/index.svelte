c1c3f1e3534861cc3c0e9f68b843cd74ed4f8b36
Stefan Buhrmester <buhrmi@gmail.com>
2021-08-18 18:09:11 +0200

    update bandsintown

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -45,7 +45,7 @@
 {/if}
 
 <div class="gigs">
-	<a class="bit-widget-initializer" data-artist-name="TWZTD" data-display-local-dates="false" data-display-past-dates="false" data-auto-style="false" data-text-color="#FFFFFF" data-link-color="#a500b4" data-background-color="rgba(0,0,0,0)" data-display-limit="15" data-display-start-time="false" data-link-text-color="#FFFFFF" data-display-lineup="true" data-display-play-my-city="true" data-separator-color="rgba(124,124,124,0.25)"></a>
+	<a class="bit-widget-initializer" data-artist-name="Jeseph" data-display-local-dates="false" data-display-past-dates="false" data-auto-style="false" data-text-color="#FFFFFF" data-link-color="#a500b4" data-background-color="rgba(0,0,0,0)" data-display-limit="15" data-display-start-time="false" data-link-text-color="#FFFFFF" data-display-lineup="true" data-display-play-my-city="true" data-separator-color="rgba(124,124,124,0.25)"></a>
 </div>
 
 <style>
